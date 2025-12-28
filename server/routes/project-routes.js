cff7ced6573d5ef7dbc63df3b6e6f1d583ae91b5
Supun Anjana <supuna793@gmail.com>
2025-12-28 08:09:32 +0530

    add matching routes to the project routes

--- server/routes/project-routes.js
+++ server/routes/project-routes.js
@@ -5,5 +5,6 @@ const projectController = require('../controllers/projectController');
 router.post('/', projectController.createProject); // To create project
 router.get('/', projectController.getAllProjects); // See the list
 router.post('/requirements', projectController.addProjectRequirement); // Skills 
+router.get('/:projectId/match', projectController.matchPersonnel); //Matching routes
 
 module.exports = router;
